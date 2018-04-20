159e67fe0eab3532bb40d240b8ccf864f8de0875
vasuthopuri <38423082+vasuthopuri@users.noreply.github.com>
2018-04-20 15:18:44 +0530

    first commit

--- website/settings.py
+++ website/settings.py
@@ -24,9 +24,9 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'qvvd_w@c1@*#&bl8w4kj%a9=&x+e(#a527t!cp!u8kc6cf9esr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
-ALLOWED_HOSTS = ["vasuthopuri.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME': 'dd66u2ep6l43b8',
-         'USER': 'yymghohylfbzbi',
-         'PASSWORD': '1b7939a115e19201f870d16742f01ebf5bcbd02aad01897255b1878978a69d33',
-         'HOST': 'ec2-54-243-213-188.compute-1.amazonaws.com',
+         'NAME': 'gd_db',
+         'USER':'postgres',
+         'PASSWORD':'vasu143',
+         'HOST': '127.0.0.1',
          'PORT': '5432',
     }
 }
@@ -123,12 +123,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-import dj_database_url
 
-DATABASES['default'] = dj_database_url.config()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
